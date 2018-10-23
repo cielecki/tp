b8f24f212430c5f7ca5e9d532e906bfb8cd7824f
Maciej Cielecki <cielecki@gmail.com>
2018-10-23 03:12:41 +0200

    Initial

---
+++ precache-manifest.c3e4575f987ff0762309b29321f55bb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0e093f7d032eef41878cc889a41ec03",
+    "url": "/tauprime/static/media/SpaceRangerTitle.a0e093f7.woff"
+  },
+  {
+    "revision": "8b44bfa557e7ed806e9b17ede1e58cde",
+    "url": "/tauprime/static/media/SpaceRangerTitle.8b44bfa5.woff2"
+  },
+  {
+    "revision": "5584c4684c92d2388eef",
+    "url": "/tauprime/static/js/runtime~main.5584c468.js"
+  },
+  {
+    "revision": "2d9b03ee4a9ff59d22a5",
+    "url": "/tauprime/static/js/main.2d9b03ee.chunk.js"
+  },
+  {
+    "revision": "95f4e893daf214117fe2",
+    "url": "/tauprime/static/js/1.95f4e893.chunk.js"
+  },
+  {
+    "revision": "2d9b03ee4a9ff59d22a5",
+    "url": "/tauprime/static/css/main.d604a87b.chunk.css"
+  },
+  {
+    "revision": "bc8b41bc288938a08e8c7991d8b364ac",
+    "url": "/tauprime/index.html"
+  }
+];
